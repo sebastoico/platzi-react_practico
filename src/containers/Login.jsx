a0da5e4cf373689c9f6ce1ef5c780aa647f58569
sebastoico <sejaramillomo@gmail.com>
2023-01-17 20:42:34 -0500

    Trasnformando HTML y CSS en componentes de React

---
+++ src/containers/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="form-container">
+        <img src="./assets/logos/logo_yard_sale.svg" alt="logo" className="logo" />
+
+        <h1 className="title">Create a new password</h1>
+        <p className="subtitle">Enter a new password for your account</p>
+
+        <form action="/" className="form">
+          <label for="password" className="label">Password</label>
+          <input type="password" id="password" placeholder="********" className="input input-password" />
+
+          <label for="new-password" className="label">Re-enter password</label>
+          <input type="password" id="new-password" placeholder="********" className="input input-password" />
+
+          <input type="submit" value="Confirm new password" className="primary-button login-button" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
