77c167ddee11b08e1527108d0a7a727d6783eeea
sebastoico <sejaramillomo@gmail.com>
2023-01-21 16:39:30 -0500

    React Context

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import '@styles/ProductItem.scss';
+import AppContext from '@context/AppContext';
 
 import cartLogo from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = ({product}) => {
-  const [cart, setCart] = useState([]);
+  const {addToCart} = useContext(AppContext);
 
-  const handleClick = () => {
-    setCart([]);
+  const handleClick = (item) => {
+    addToCart(item);
   }
 
   return (
@@ -18,7 +19,7 @@ const ProductItem = ({product}) => {
           <p>$ {product.price}.00</p>
           <p>{product.title}</p>
         </div>
-        <figure onClick={handleClick}>
+        <figure onClick={() => handleClick(product)}>
           <img src={cartLogo} alt="" />
         </figure>
       </div>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,26 +11,32 @@ import CreateAccount from "../pages/CreateAccount.jsx";
 import Checkout from "../pages/Checkout.jsx";
 import Orders from "../pages/Orders.jsx";
 import NotFound from "../pages/NotFound.jsx";
+import AppContext from "../context/AppContext.js";
+import useInitialState from "../hooks/useInitialState.js";
 import "@styles/global.css";
 
 const App = () => {
+  const initialState = useInitialState();
+
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/password-recovery" component={PasswordRecovery} />
-          <Route exact path="/send-email" component={SendEmail} />
-          <Route exact path="/new-password" component={NewPassword} />
-          <Route exact path="/account" component={MyAccount} />
-          <Route exact path="/signup" component={CreateAccount} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/orders" component={Orders} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/password-recovery" component={PasswordRecovery} />
+            <Route exact path="/send-email" component={SendEmail} />
+            <Route exact path="/new-password" component={NewPassword} />
+            <Route exact path="/account" component={MyAccount} />
+            <Route exact path="/signup" component={CreateAccount} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/orders" component={Orders} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
